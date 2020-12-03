1710b6c6add4fae9c4996b68e82cc4fb43111d88
miguel-salazar-utags <58369142+miguel-salazar-utags@users.noreply.github.com>
2020-12-03 17:01:29 -0600

    Creada la cadena de conexion con mongo atlas en la nube

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(require('./routes/usuario'));
 app.use(require('./routes/categoria'));
 app.use(require('./routes/login'));
 
-mongoose.connect('mongodb://localhost:27017/cafeteria', {
+mongoose.connect('mongodb+srv://admin:12345678Qq@cluster0.0mdsl.mongodb.net/cafeteria', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
